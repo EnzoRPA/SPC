5f8a183b494574ae1acac9ef64288634f246a0f9
enzo <enzo.ubber@gmail.com>
2025-12-30 09:38:51 -0300

    Force redeploy with debug

--- api/index.php
+++ api/index.php
@@ -1,2 +1,6 @@
 <?php
+// Debug to verify deploy
+// echo "Deploy version: 1.0.1<br>";
+// echo "Current dir: " . __DIR__ . "<br>";
+// echo "Vendor path: " . realpath(__DIR__ . '/../vendor/autoload.php') . "<br>";
 require __DIR__ . '/../public/index.php';
